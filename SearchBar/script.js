655911bd973f0a6dc6b830ec5174ace28bea40ad
Shivahir2003 <shivahir0114@gmail.com>
2023-11-09 23:50:11 +0530

    "new Project added"

---
+++ SearchBar/script.js
@@ -0,0 +1,26 @@
+const search =document.querySelector('.search');
+const searchBox =document.querySelector('.search-box');
+const searchBtn =document.querySelector('.search-btn');
+const closeBtn =document.querySelector('.close-btn');
+
+searchBtn.addEventListener('click', function(){
+    if(search.classList.contains('active'))
+    {
+        searchBox.value = '';
+    }
+    // else{
+    // search.classList.add('active');
+    // }
+})
+
+searchBtn.addEventListener('mouseover', function(){
+    search.classList.add('active');
+    searchBox.focus();
+})
+
+closeBtn.addEventListener('click', function(){
+    search.classList.remove('active');
+    searchBox.value = '';
+
+
+})
